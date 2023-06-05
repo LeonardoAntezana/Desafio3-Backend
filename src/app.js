a315d0a4598233ef1a52b32fe18b3d0947aef13e
¨Leonardo <leonardoantezana59@gmail.com>
2023-06-05 13:43:22 -0300

    implementando rutas

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { ProductManager } from "./ProductManager.js";
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+const manager1 = new ProductManager('Leonardo', './products.json');
+
+app.get('/products', async (req, res) => {
+  const products = await manager1.getProducts();
+  const { limit } = req.query;
+  if(!limit){
+    return res.send({products: products});
+  }
+  const filter = products.slice(0, limit);
+  res.send({products: filter})
+})
+
+app.get('/products/:pid', async (req, res) => {
+  const products = await manager1.getProducts();
+  const id = req.params.pid;
+  const productFind = products.find(prod => prod.id == id);
+  if(productFind){
+    return res.send({productFind: productFind});
+  }
+  res.send('Producto no encontrado')
+})
+
+app.listen(8080, console.log('Escuchando puerto 8080'))
